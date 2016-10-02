b0fb6446883b236dd2783c24432039ab758e3300
Fady Barsoum <git.committer@fadybarsoum.com>
2016-10-02 01:41:19 -0400

    Initial Commit of RIP Implementation
    and associated config file. The protocol file is currently basically empty

---
+++ src/rip/RIPProtocol.py
@@ -0,0 +1,11 @@
+'''
+Implementation of The Reliable Interaction Protocol (RIP)
+as drafted by JHU's Fall 2016 Network Security class's PETF
+Transport Layer on the Playground network
+Implemented using Twisted 16.4.1
+
+Author: Fady Barsoum
+Created: 02OCT2016 1:05AM
+Last Modified: 02OCT2016 1:05AM
+'''
+
